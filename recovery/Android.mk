b37a82d28419e9c037e251788e770e24aad94c81
Michael Bestas <mkbestas@lineageos.org>
2019-01-22 23:01:59 +0200

    Revert "recovery: Use project pathmap for recovery lib"
    This reverts commit 32e78d8e12c8a8896fbf5d8ec1713f0aa1838288.
    
    Change-Id: I3a524b638179e875d65e5bd9e80a49abd8d70dde

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -2,10 +2,8 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_C_INCLUDES := \
-    bootable/recovery \
     bootable/recovery/edify/include \
-    bootable/recovery/otautil/include 
-
+    bootable/recovery/otautil/include
 LOCAL_SRC_FILES := recovery_updater.cpp
 LOCAL_MODULE := librecovery_updater_cm
 LOCAL_MODULE_TAGS := eng
